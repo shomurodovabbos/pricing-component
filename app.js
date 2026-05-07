96ef427b76d49f7105074958c16cf683c5a77428
abbos91 <abbos.shomurodov91@gmail.com>
2026-05-07 16:56:40 +0500

    discount functionality updated

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 const slider = document.getElementById("slider");
 const price = document.getElementById("price");
 const toggle = document.querySelector(".toggle input");
+
 const prices = [0, 8, 16, 24, 30];
 
 function updateUI() {
     const value = Number(slider.value);
 
+    let finalPrice = prices[value];
+
+    if (toggle.checked) {
+        finalPrice *= 0.25;
+    }
+
     // PRICE
-    price.textContent = `$${prices[value].toFixed(2)}`;
+    price.textContent = `$${finalPrice.toFixed(2)}`;
 
     // PROGRESS
     const percent = (value / slider.max) * 100;
 
-    slider.style.background = `linear-gradient(to right,
-    var(--green-400) ${percent}%,
-    var(--slate-200) ${percent}%
-)`;
+    slider.style.background = `
+        linear-gradient(
+            to right,
+            var(--green-400) ${percent}%,
+            var(--slate-200) ${percent}%
+        )
+    `;
 }
 
 slider.addEventListener("input", updateUI);
+toggle.addEventListener("change", updateUI);
 
-toggle.addEventListener("change", () => {
-    if (toggle.checked) {
-        const value = Number(slider.value);
-        const discounted = prices[value] * 0.25;
-        price.textContent = `$${discounted.toFixed(2)}`;
-    } else {
-        const value = Number(slider.value);
-        price.textContent = `$${prices[value].toFixed(2)}`;
-    }
-});
 // initial state
 slider.value = 2;
 toggle.checked = false;
+
 updateUI();
